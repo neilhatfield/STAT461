c9254ce46cdb09185157e072c417bc9c611cb74d
Neil Hatfield <51502396+neilhatfield@users.noreply.github.com>
2022-08-10 14:02:15 -0400

    Update checkSetup.R
    Updates for fall 2022 package list

--- rScripts/checkSetup.R
+++ rScripts/checkSetup.R
@@ -1,52 +1,69 @@
 checkSetup <- function(){
-
   ## Check R Version ----
-  currentVersion <- "4.1.2"
+  currentVersion <- "4.2.1"
   errorMessages <- c()
   if (as.numeric(version$major) < 4) {
     stop("Your R version is too far out of date. Update before continuing.")
   } else if (paste(version$major, version$minor, sep = ".") != currentVersion) {
-    warning("You should update to version 4.1.2 when possible.")
+    warning("You should update your R when possible.")
   } else {
-    print("R Version is good.")
+    print("R Version is current.")
+  }
+  
+  ## Define package lists ----
+  corePackages <- c("tidyverse", "knitr", "kableExtra", "car", "psych",
+                   "parameters", "rcompanion", "DescTools", "dunn.test",
+                    "multcompView", "emmeans", "rstatix", "lme4", 
+                    "nlme", "tinytex", "rmarkdown")
+  optionalPackages <- c("lattice", "perm", "boot", "coin", "pwr", "WebPower", "openxlsx")
+
+  ## Package installation ----
+  pkgInstall <- menu(
+    choices = c("Install Everything", "Install Only Required", "Skip Package Install"),
+    title = "What packages do you want installed?"
+  )
+  if (pkgInstall == 1) {
+    pkgList <- c(corePackages, optionalPackages)
+  } else if (pkgInstall == 2) {
+    pkgList <- corePackages
+  } else if {
+    pkgList <- NA
   }
 
-  ## Check installed packages ----
-  print("Checking for installed packages...")
-  reqPackages <- c("tidyverse", "knitr", "kableExtra", "car", "sjstats", "psych",
-                   "parameters", "lattice", "lme4", "DescTools", "afex", "emmeans",
-                   "rstatix", "rsm", "coin", "rcompanion", "boot", "lmboot",
-                   "multicool", "dunn.test", "tinytex", "rmarkdown", "perm"
-                   )
-  newPackages <- reqPackages[!(reqPackages %in% installed.packages()[,"Package"])]
-  if (length(newPackages)) {install.packages(newPackages)}
+  if (!is.na(pkgList)) {
+    ## Check installed packages ----
+    print("Checking for installed packages...")
+    newPackages <- pkgList[!(pkgList %in% installed.packages()[,"Package"])]
+    if (length(newPackages)) {install.packages(newPackages)}
+  
+    ### Check for Bioconductor ----
+    if (!requireNamespace("BiocManager", quietly = TRUE)) {
+      install.packages("BiocManager")
+    }
+    if (!("Rgraphviz" %in% installed.packages()[, "Package"])) {
+      BiocManager::install("Rgraphviz")
+    }
 
-  ### Check for Bioconductor ----
-  if (!requireNamespace("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
-  }
-  if (!("Rgraphviz" %in% installed.packages()[, "Package"])) {
-    BiocManager::install("Rgraphviz")
-  }
+    ### Install hasseDiagrams package ----
+    if (!("hasseDiagram" %in% installed.packages()[, "Package"])) {
+      install.packages("hasseDiagram")
+    }
 
-  ### Install hasseDiagrams package ----
-  if (!("hasseDiagram" %in% installed.packages()[, "Package"])) {
-    install.packages("hasseDiagram")
-  }
-
-  print("Finished installing packages")
-
-  ## Tex Installation
-  texInstall <- "prompt"
-  while (!(texInstall %in% c("yes", "y", "no", "n"))) {
-    texInstall <- readline(prompt = "Do you want to install TeX/LaTex? Yes/No: ")
-    texInstall <- tolower(texInstall)
+    print("Finished installing packages")
   }
 
-  if (texInstall == "yes" || texInstall == "y") {
-    print("Installing TinyTeX...")
-    tinytex::install_tinytex()
-    print("Finished installing TinyTex")
+  if (pkgInstall > 0) {
+    ## Tex Installation
+    texInstall <- menu(
+      choices = c("Yes", "No"),
+      title = "Do you want to install Tex/LaTeX ?"
+    )
+
+    if (texInstall == 1) {
+      print("Installing TinyTeX...")
+      tinytex::install_tinytex()
+      print("Finished installing TinyTex")
+    }
   }
 
   print("All finished. Your computer is ready for Stat 461!")
