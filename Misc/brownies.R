4f01f7233e0f9e30b10d3d4100d9af33ec382a13
neilhatfield <phiguy1114@gmail.com>
2023-04-19 21:42:07 -0400

    Brownies and Beer Updates

--- Misc/brownies.R
+++ Misc/brownies.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(googlesheets4)
 library(lme4)
 library(rstatix)
+library(blme)
 
 # Load Data ----
 googlesheets4::gs4_deauth()
@@ -13,9 +14,6 @@ tasteDataWide <- googlesheets4::read_sheet(
   col_names = TRUE
 )
 
-tasteSec2 <- tasteDataWide[10:18, ]
-tasteSec3 <- tasteDataWide[1, ]
-
 ## Make a long version of the data
 tasteDataLong <- tasteDataWide %>%
   pivot_longer(
@@ -23,23 +21,119 @@ tasteDataLong <- tasteDataWide %>%
     names_to = "Mix",
     names_transform = list(Mix = as.factor),
     values_to = "score"
+  ) %>%
+  rowwise() %>%
+  mutate(
+    Order = str_locate(
+      string = Order,
+      pattern = gsub(
+        pattern = "Mix ",
+        replacement = "",
+        x = Mix)
+    )[1]
   )
-tasteDataLong$adjScore <- tasteDataLong$score + rnorm(nrow(tasteDataLong), mean = 0, sd = 0.25)
 
-taste2Long <- tasteSec2 %>%
-  pivot_longer(
-    cols = starts_with("Mix"),
-    names_to = "Mix",
-    names_transform = list(Mix = as.factor),
-    values_to = "score"
+# Data Exploration ----
+## Univariate Plots ----
+### Histogram of Scores ----
+ggplot(
+  data = tasteDataLong,
+  mapping = aes(x = score)
+) +
+  geom_histogram(
+    color = "black",
+    fill = boastUtils::psuPalette[1],
+    bins = 9,
+    boundary = 0,
+    closed = "left"
+  ) +
+  theme_bw() +
+  scale_y_continuous(
+    expand = expansion(mult = 0, add = c(0, 2))
+  ) +
+  scale_x_continuous(
+    breaks = 1:10
+  ) +
+  theme(
+    text = element_text(size = 18)
   )
 
-taste3Long <- tasteSec3 %>%
-  pivot_longer(
-    cols = starts_with("Mix"),
-    names_to = "Mix",
-    names_transform = list(Mix = as.factor),
-    values_to = "score"
+### Shadowgram of Scores ----
+source("https://raw.github.com/neilhatfield/STAT461/master/rScripts/shadowgram.R")
+shadowgram(
+  dataVec = tasteDataLong$score,
+  label = "Hedonic Score",
+  color = "darkblue"
+) +
+  theme(
+    text = element_text(size = 18)
+  )
+
+## Bivariate Plots ----
+### Box plot of Score by Mix ----
+ggplot(
+  data = tasteDataLong,
+  mapping = aes(x = Mix, y = score)
+) +
+  geom_boxplot() +
+  theme_bw() +
+  theme(
+    text = element_text(size = 18)
+  )
+
+### Box plot of Score by Taster ----
+ggplot(
+  data = tasteDataLong,
+  mapping = aes(x = Taster, y = score)
+) +
+  geom_boxplot() +
+  theme_bw() +
+  theme(
+    text = element_text(size = 18)
+  ) +
+  scale_x_discrete(
+    labels = scales::label_wrap(10),
+    guide = guide_axis(angle = 45)
+  )
+
+## Multivariate Plots ----
+### Heat Map ----
+ggplot(
+  data = tasteDataLong,
+  mapping = aes(x = Taster, y = Mix, weight = score)
+) +
+  geom_bin_2d() +
+  theme_bw() +
+  scale_x_discrete(
+    labels = scales::label_wrap(10),
+    guide = guide_axis(angle = 45)
+  ) +
+  scale_fill_gradient2(name = "Score") +
+  theme(
+    text = element_text(size = 18)
+  )
+
+### Alluvial Plot ----
+# EXPERIMENTAL
+library(ggalluvial)
+ggplot(
+  data = tasteDataLong,
+  mapping = aes(y = score, axis1 = Mix, axis2 = Taster, axis3 = score)
+) +
+  geom_alluvium(aes(fill = Mix)) +
+  geom_stratum(fill = "white", color = "gray") +
+  geom_label(stat = "stratum", aes(label = after_stat(stratum))) +
+  theme_void() +
+  scale_x_discrete(
+    limits = c("Mix", "Taster", "Score"),
+    expand = expansion()
+    ) +
+  theme(
+    text = element_text(size = 18),
+    axis.text.y = element_blank(),
+    axis.title.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.position = "none"
   )
 
 # Fit Models ----
@@ -49,27 +143,34 @@ brownieOmni <- aov(
   data = tasteDataLong
 )
 
-## Random Effects ----
+## Random Effects (Assumption Checking) ----
 brownieMixed <- lme4::lmer(
   formula = score ~ (1|Taster) + Mix,
   data = tasteDataLong
 )
+
 ### Due to singularity, using a Bayesian approach for estimation
-bayesianMixed <- rstanarm::stan_glmer(
+bayesMixed <- blme::blmer(
   formula = score ~ (1|Taster) + Mix,
-  data = tasteDataLong
+  data = tasteDataLong,
+  cov.prior = wishart
 )
 
-## Spherecity ----
+## Model for checking Spherecity ----
 brownieSphere <- rstatix::anova_test(
   data = tasteDataLong,
   formula = score ~ Mix + Error(Taster %in% Mix)
 )
 
+## Informal Interaction Check
+# Note: you can't fit an interaction model to do an informal
+# test of the interaction term like we can in ANCOVA as
+# we run out of Degrees of Freedom and can't estimate terms
+
 # Assumption Checking -----
 ## Gaussian Residuals ----
 car::qqPlot(
-  x = residuals(brownieMixed),
+  x = residuals(bayesMixed), ## Notice which model is getting used
   distribution = "norm",
   envelope = 0.90,
   id = FALSE,
@@ -79,9 +180,9 @@ car::qqPlot(
 
 ## Gaussian Taster Effect ----
 car::qqPlot(
-  x = unlist( 
+  x = unlist(
     lme4::ranef(
-      object = bayesianMixed, ## Notice which model is getting used
+      object = bayesMixed, ## Notice which model is getting used
       whichel = c("Taster")
     )
   ),
@@ -95,8 +196,8 @@ car::qqPlot(
 ## Homoscedasticity ----
 ggplot(
   data = data.frame(
-    residuals = residuals(brownieMixed), # Notice which model
-    fitted = fitted.values(brownieMixed)
+    residuals = residuals(bayesMixed), ## Notice which model is getting used
+    fitted = fitted.values(bayesMixed)
   ),
   mapping = aes(x = fitted, y = residuals)
 ) +
@@ -117,51 +218,60 @@ ggplot(
   xlab("Fitted values (score)") +
   ylab("Residuals (score)")
 
+cbind(
+  tasteDataLong,
+  residuals = residuals(bayesMixed) ## Notice which model is getting used
+) %>%
+  ggplot(
+    mapping = aes(x = Order, y = residuals)
+  ) +
+  geom_point(
+    # mapping = aes(shape = Mix, color = Mix),
+    size = 5
+  ) +
+  geom_line() +
+  geom_hline(
+    yintercept = 0,
+    linetype = "dashed",
+    color = "grey50"
+  ) +
+  theme_bw() +
+  xlab("Tasting Order") +
+  ylab("Residuals (score)") +
+  facet_wrap(facets = vars(Taster)) +
+  scale_color_manual(values = boastUtils::psuPalette[c(1:4,6)]) +
+  theme(text = element_text(size = 18))
+
 ## Interaction ----
+sec2 <- c("Mike", "One", "SpaceJam", "Tester", "silly-hedgehog-69",
+          "Gordon Ramsay", "CoconutHead", "The terminator (Joseph)", "Jisoo Kim")
+sec3 <- c("Kenny", "Bob", "Taylor", "Jenn", "BEARBERRY", "Karen", "Todd Curtis",
+          "Sara", "Antonio Brown")
 tasteDataLong %>%
   mutate(
-    tempGroup = case_when(
-      Taster %in% c("Kenny", "The terminator (Joseph)") ~ "G1",
-      Taster %in% c("Karen", "SpaceJam") ~ "G2",
-      Taster %in% c("Todd Curtis", "Jisoo Kim") ~ "G3",
-      Taster %in% c("BEARBERRY", "Gordon Ramsay") ~ "G4",
-      Taster %in% c("Sara", "CoconutHead") ~ "G5",
-      Taster %in% c("Antonio Brown", "One") ~ "G6",
-      Taster %in% c("Bob", "silly-hedgehog-69") ~ "G7",
-      Taster %in% c("Jenn", "Mike") ~ "G8",
-      Taster %in% c("Taylor", "Tester") ~ "G9",
+    section = case_when(
+      Taster %in% sec2 ~ "Sec. 2",
+      Taster %in% sec3 ~ "Sec. 3",
+      .default = "Error"
     )
   ) %>%
-ggplot(
-  mapping = aes(
-    x = Taster,
-    y = score,
-    color = Mix,
-    group = Mix
-  )
-) +
-  geom_point(size = 2) +
-  geom_line() +
-  ggplot2::theme_bw() +
-  xlab("Taster") +
-  ylab("Score") +
-  labs(
-    color = "Mix"
+  ggplot(
+    mapping = aes(x = Mix, y = score, group = Taster, color = Taster)
   ) +
-  scale_color_manual(values = boastUtils::boastPalette) +
-  facet_wrap(
-    facets = vars(tempGroup),
-    nrow = 3,
-    ncol = 3,
-    scales = "free_x"
+  geom_point() +
+  geom_line() +
+  theme_bw() +
+  facet_wrap(facets = vars(section)) +
+  theme(
+    legend.position = "none"
   )
-
 
 ## Sphericity ----
+source("https://raw.github.com/neilhatfield/STAT461/master/rScripts/ANOVATools.R")
 sphericityPlot(
   dataWide = tasteDataWide, # Data needs to be in wide format
-  subjectID = "Taster", 
-  colors = "deafult", 
+  subjectID = "Taster",
+  colors = "default",
 )
 
 brownieSphere$`Mauchly's Test for Sphericity` %>%
