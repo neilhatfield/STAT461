a858645df6a0bbff72cb606449529087f77fc09c
neilhatfield <phiguy1114@gmail.com>
2024-03-22 09:26:36 -0400

    Accessibility updates and plane code

--- codingRGuides/Unit 3 Guides/usingScreens.R
+++ codingRGuides/Unit 3 Guides/usingScreens.R
@@ -96,7 +96,7 @@ screenCalc2 <- plane2_Screens %>%
     )
   )
 
-# ANOVA Model Check
+# ANOVA Model Check ----
 plane2Model <- aov(
   formula = distance ~ design,
   data = plane2,
@@ -110,3 +110,110 @@ plane3Model <- aov(
   na.action = "na.omit"
 )
 anova(plane3Model)
+
+# Assumption Checking ----
+## Sec. 2 ----
+### Gaussian Residuals ----
+car::qqPlot(
+  x = plane2Model$residuals,
+  distribution = "norm",
+  envelope = 0.90,
+  id = FALSE,
+  pch = 20,
+  ylab = "Residuals (in)",
+  main = "Sec. 2's Air Plane Study Residual QQ Plot"
+)
+psych::skew(plane2Model$residuals)
+psych::kurtosi(plane2Model$residuals)
+
+### Homoscedasicity ----
+ggplot(
+  data = data.frame(
+    residuals = plane2Model$residuals,
+    fitted = plane2Model$fitted.values
+  ),
+  mapping = aes(x = fitted, y = residuals)
+) +
+  geom_point(size = 2) +
+  theme_bw() +
+  labs(
+    x = "Fitted values (in)",
+    y = "Residuals (in)",
+    title = "Sec. 2's Strip Chart for the Paper Airplane Study"
+  )
+
+### Independent Obs. ----
+ggplot(
+  data = data.frame(
+    residuals = plane2Model$residuals,
+    index = 1:length(plane2Model$residuals)
+  ),
+  mapping = aes(x = index, y = residuals)
+) +
+  geom_point(size = 1.5) +
+  geom_line() +
+  theme_bw() +
+  geom_hline(
+    yintercept = 0,
+    linetype = "dashed",
+    color = "red"
+  ) +
+  labs(
+    x = "Measurement order",
+    y = "Residuals (in)",
+    title = "Sec. 2's Index Plot for the Paper Airplane Study"
+  )
+car::durbinWatsonTest(plane2Model)$dw
+
+## Sec. 3 ----
+### Gaussian Residuals ----
+car::qqPlot(
+  x = plane3Model$residuals,
+  distribution = "norm",
+  envelope = 0.90,
+  id = FALSE,
+  pch = 20,
+  ylab = "Residuals (in)",
+  main = "Sec. 3's Air Plane Study Residual QQ Plot"
+)
+psych::skew(plane3Model$residuals)
+psych::kurtosi(plane3Model$residuals)
+
+### Homoscedasicity ----
+ggplot(
+  data = data.frame(
+    residuals = plane3Model$residuals,
+    fitted = plane3Model$fitted.values
+  ),
+  mapping = aes(x = fitted, y = residuals)
+) +
+  geom_point(size = 2) +
+  theme_bw() +
+  labs(
+    x = "Fitted values (in)",
+    y = "Residuals (in)",
+    title = "Sec. 3's Strip Chart for the Paper Airplane Study"
+  )
+
+### Independent Obs. ----
+ggplot(
+  data = data.frame(
+    residuals = plane3Model$residuals,
+    index = 1:length(plane3Model$residuals)
+  ),
+  mapping = aes(x = index, y = residuals)
+) +
+  geom_point(size = 1.5) +
+  geom_line() +
+  theme_bw() +
+  geom_hline(
+    yintercept = 0,
+    linetype = "dashed",
+    color = "red"
+  ) +
+  labs(
+    x = "Measurement order",
+    y = "Residuals (in)",
+    title = "Sec. 3's Index Plot for the Paper Airplane Study"
+  )
+car::durbinWatsonTest(plane3Model)$dw
